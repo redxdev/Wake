3997f5c035a20a2f2ff59ca00424233d8cde387c
Sam Bloomberg <sam@redxdev.com>
2015-09-22 18:20:11 -0700

    fix

--- ci-install.sh
+++ ci-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-if [! -d "$HOME/cmake/bin" ]; then
+if [ ! -d "$HOME/cmake/bin" ]; then
     wget http://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz -O /tmp/cmake.tar.gz;
     tar -xvf /tmp/cmake.tar.gz;
     mv cmake-3.3.2-Linux-x86_64 cmake;
