da145ad63332dd5b0f5d61ccb66db737a0e372e9
Sam Bloomberg <sam@redxdev.com>
2015-09-22 18:51:30 -0700

    fix travis again

--- ci-install.sh
+++ ci-install.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 set -e
-if [ ! -d "$HOME/cmake/bin" ]; then
-    wget http://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz -O /tmp/cmake.tar.gz;
-    tar -xvf /tmp/cmake.tar.gz;
-    mv cmake-3.3.2-Linux-x86_64 cmake;
-else
-    echo 'Using cached cmake';
-fi
 
-if [ ! -d "$HOME/glm" ]; then
-    wget https://github.com/g-truc/glm/releases/download/0.9.7.1/glm-0.9.7.1.zip -O /tmp/glm.zip;
-    unzip /tmp/glm.zip;
-else
-    echo 'Using cached GLM';
-fi
+wget http://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz -O /tmp/cmake.tar.gz;
+tar -xvf /tmp/cmake.tar.gz;
+mv cmake-3.3.2-Linux-x86_64 cmake;
+
+wget https://github.com/g-truc/glm/releases/download/0.9.7.1/glm-0.9.7.1.zip -O /tmp/glm.zip;
+unzip /tmp/glm.zip;
