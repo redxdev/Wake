ca567dd6ba570e4515d86f322de1ad906407752c
Sam Bloomberg <sam@redxdev.com>
2015-09-24 21:07:17 -0700

    fix build

--- ci-install.sh
+++ ci-install.sh
@@ -17,7 +17,7 @@ fi
 
 if [ ! -d "$HOME/glfw/src" ]; then
     wget https://github.com/glfw/glfw/releases/download/3.1.1/glfw-3.1.1.zip -O /tmp/glfw.zip;
-    unzip /tmp/glfw -d $HOME;
+    unzip /tmp/glfw -d .;
     mv glfw-3.1.1 glfw;
     ./cmake/bin/cmake glfw;
     cd glfw
