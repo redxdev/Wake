2b3987cfba4c766054d3e56f66f3c4cff8aa2d13
Sam Bloomberg <sam@redxdev.com>
2016-02-29 16:48:35 -0500

    quiet travis stuff, add build status indicator to readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Wake
 
+[![Build Status](https://travis-ci.org/redxdev/Wake.svg?branch=master)](https://travis-ci.org/redxdev/Wake)
+
 Wake is a small game engine written in C++ and Lua.
 
 ## License
--- ci-install-deps.sh
+++ ci-install-deps.sh
@@ -4,13 +4,13 @@ mkdir deps-install
 cd deps-install
 
 wget https://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.tar.gz
-tar xvzf cmake-3.4.3-Linux-x86_64.tar.gz
+tar xzf cmake-3.4.3-Linux-x86_64.tar.gz
 cd cmake-3.4.3-Linux-x86_64
 sudo cp -R * /
 cd ..
 
 wget https://github.com/glfw/glfw/releases/download/3.1.2/glfw-3.1.2.zip
-unzip glfw-3.1.2.zip
+unzip -q glfw-3.1.2.zip
 cd glfw-3.1.2
 cmake -DBUILD_SHARED_LIBS=0 -DGLFW_BUILD_TESTS=0 -DGLFW_BUILD_DOCS=0 -DGLFW_BUILD_EXAMPLES=0 .
 make
@@ -18,7 +18,7 @@ sudo make install
 cd ..
 
 wget https://github.com/g-truc/glm/releases/download/0.9.7.3/glm-0.9.7.3.zip
-unzip glm-0.9.7.3.zip
+unzip -q glm-0.9.7.3.zip
 cd glm
 cmake .
 make
