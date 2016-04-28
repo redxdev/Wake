721fb13a85c0f37d75afaeab6d7e6c18d679adfd
Sam Bloomberg <sam@redxdev.com>
2016-04-27 20:35:34 -0400

    better test for perspective

--- dist/tests/native/math.lua
+++ dist/tests/native/math.lua
@@ -4,8 +4,8 @@ local math = math
 test.suite('Math Extension Library')
 
 test.test('perspective', function()
-    local pers = math.perspective(45, 4/3, 0, 1000)
-    local expected = {1.344443, 0, 0, 0, 0, 1.792591, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0 }
+    local pers = math.perspective(45, 4/3, 0.1, 1000)
+    local expected = {1.344443, 0, 0, 0, 0, 1.792591, 0, 0, 0, 0, -1.0002, -1, 0, 0, -0.20002, 0 }
     local actual = pers:table()
     for i=1,#expected do
         test.expect_num_equal(expected[i], actual[i], 0.000001)
