4c4f7e6691107125d441f35fcdf3da33fe534703
Sam Bloomberg <sam@redxdev.com>
2016-05-24 16:30:38 -0400

    only use cmake and make if the files didn't exist

--- ci-install-deps.sh
+++ ci-install-deps.sh
@@ -16,10 +16,12 @@ if [ ! -d glfw-3.1.2 ]
 then
     wget https://github.com/glfw/glfw/releases/download/3.1.2/glfw-3.1.2.zip
     unzip -q glfw-3.1.2.zip
+    cd glfw-3.1.2
+    cmake -DBUILD_SHARED_LIBS=0 -DGLFW_BUILD_TESTS=0 -DGLFW_BUILD_DOCS=0 -DGLFW_BUILD_EXAMPLES=0 .
+    make
+else
+    cd glfw-3.1.2
 fi
-cd glfw-3.1.2
-cmake -DBUILD_SHARED_LIBS=0 -DGLFW_BUILD_TESTS=0 -DGLFW_BUILD_DOCS=0 -DGLFW_BUILD_EXAMPLES=0 .
-make
 sudo make install
 cd ..
 
@@ -27,10 +29,12 @@ if [ ! -d glm ]
 then
     wget https://github.com/g-truc/glm/releases/download/0.9.7.3/glm-0.9.7.3.zip
     unzip -q glm-0.9.7.3.zip
+    cd glm
+    cmake .
+    make
+else
+    cd glm
 fi
-cd glm
-cmake .
-make
 sudo make install
 cd ..
 
@@ -38,10 +42,12 @@ if [ ! -d assimp-3.2 ]
 then
     wget -O assimp-3.2.zip https://github.com/assimp/assimp/archive/v3.2.zip
     unzip -q assimp-3.2.zip
+    cd assimp-3.2
+    cmake -DASSIMP_BUILD_SAMPLES=false -DASSIMP_BUILD_TESTS=false -DASSIMP_BUILD_ASSIMP_TOOLS=false .
+    make
+else
+    cd assimp-3.2
 fi
-cd assimp-3.2
-cmake -DASSIMP_BUILD_SAMPLES=false -DASSIMP_BUILD_TESTS=false -DASSIMP_BUILD_ASSIMP_TOOLS=false .
-make
 sudo make install
 cd ..
 
@@ -49,11 +55,13 @@ if [ ! -d snappy-1.1.3 ]
 then
     wget -O snappy-1.1.3.zip https://github.com/google/snappy/archive/1.1.3.zip
     unzip -q snappy-1.1.3.zip
+    cd snappy-1.1.3
+    ./autogen.sh
+    ./configure
+    make
+else
+    cd snappy-1.1.3
 fi
-cd snappy-1.1.3
-./autogen.sh
-./configure
-make
 sudo make install
 cd ..
 
